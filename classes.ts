efbf46d0e0babe3266b472ca69b9d71d64995e24
DaniloMSilveira <danilomunifafibe@hotmail.com>
2021-08-09 22:50:38 -0300

    Subindo conteúdo das aulas 1 ~ 13

---
+++ classes.ts
@@ -0,0 +1,49 @@
+
+abstract class UserAccount {
+    public name: string
+    protected age: number
+
+    constructor(name: string, age: number) {
+        this.name = name
+        this.age = age
+    }
+
+    logDetails(): void {
+        console.log(`The player ${this.name} is ${this.age} years old.`)
+    }
+}
+
+class CharAccount extends UserAccount {
+    private nickname: string
+    private level: number
+
+    constructor(name: string, age: number, nickname: string, level: number) {
+        super(name, age)
+
+        this.nickname = nickname
+        this.level = level
+    }
+
+    get getLevel() {
+        return this.level
+    }
+
+    set setLevel(level: number) {
+        this.level = level
+    }
+
+    logCharDetails(): void {
+        console.log(`The player ${this.name} is ${this.age} years old and has the char ${this.nickname} at level ${this.level}.`)
+    }
+}
+
+// const will = new UserAccount("Willian", 30)
+// console.log(will)
+// will.logDetails()
+
+const john = new CharAccount("John", 45, "johnmaster", 80)
+console.log(john)
+john.logCharDetails()
+john.setLevel = 499
+
+console.log(john.getLevel)
---
+++ user.ts
@@ -0,0 +1,36 @@
+
+// accountInfo
+// charInfo
+// PlayerInfo
+
+type AccountInfo = {
+    id: number,
+    name: string,
+    email?: string
+}
+
+const account: AccountInfo = {
+    id: 123,
+    name: "Willian"
+}
+
+type CharInfo = {
+    nickname: string,
+    level: number
+}
+
+const char: CharInfo = {
+    nickname: "willjusten",
+    level: 100
+}
+
+// intersection
+type PlayerInfo = AccountInfo & CharInfo
+
+const player: PlayerInfo = {
+    id: 123,
+    name: "willian",
+    nickname: "willjusten",
+    level: 100
+}
+
